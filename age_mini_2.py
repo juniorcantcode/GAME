2aecb3ee5824d748a7e26979e67d5c79579bc9c5
Nihilanthrope <rebux690@gmail.com>
2026-05-24 02:38:19 +0530

    Add files via upload

---
+++ age_mini_2.py
@@ -0,0 +1,6 @@
+age = int(input("Please Enter Your Age :- "))
+
+if age>=18:
+    print (" You Are eligible To Vote")
+else:
+    print(f"Your Are Not eligible To vote You Have To Wait {18-age} Years")
